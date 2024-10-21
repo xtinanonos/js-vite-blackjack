2bb740592dd0ac38dbaecbd52e12f7124024fb59
Cristina Bolaños <148043481+xtinanonos@users.noreply.github.com>
2024-10-21 14:09:45 +0000

    vite.config added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  publicDir: 'public', // Asegúrate de que esta ruta sea correcta
+    base: '/js-vite-blackjack/'
 });
